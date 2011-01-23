e0ffc399e7ef3e5c23b1bea16c5d91149cec27f1
Mindaugas Mozuras <mindaugas.mozuras@gmail.com>
2011-01-24 00:17:51 +0200

    Initial commit

---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LinqToGmail")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LinqToGmail")]
+[assembly: AssemblyProduct("LinqToGmail")]
+[assembly: AssemblyCopyright("Copyright © Mindaugas Mozūras 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("558D6CAA-273E-11E0-8EF6-2DF4DED72085")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
