7b5fa16e1354e8d50202e734c75324b1acc47de7
Mindaugas Mozuras <mindaugas.mozuras@gmail.com>
2011-02-05 12:58:42 +0200

    A couple minor fixes

--- src/LinqToGmail/Imap/Commands/ICommand.cs
+++ src/LinqToGmail/Imap/Commands/ICommand.cs
@@ -2,7 +2,6 @@ namespace LinqToGmail.Imap.Commands
 {
     public interface ICommand
     {
-        //TODO: Needs a better name.
         string Text { get; }
     }
 }
--- src/LinqToGmail/Query/GmailQueryModelVisitor.cs
+++ src/LinqToGmail/Query/GmailQueryModelVisitor.cs
@@ -33,37 +33,37 @@
                 Results = imapSslClient.Execute<IEnumerable<MailboxMessage>>(command);
             }
             else if (resultOperator is AverageResultOperator)
-                throw new NotSupportedException("LinqToExcel does not provide support for the Average() method");
+                throw new NotSupportedException("LinqToGmail does not provide support for the Average() method");
             else if (resultOperator is CountResultOperator)
-                throw new NotSupportedException("LinqToExcel does not provide support for the Count() method");
+                throw new NotSupportedException("LinqToGmail does not provide support for the Count() method");
             else if (resultOperator is LongCountResultOperator)
-                throw new NotSupportedException("LinqToExcel does not provide support for the LongCount() method");
+                throw new NotSupportedException("LinqToGmail does not provide support for the LongCount() method");
             else if (resultOperator is FirstResultOperator)
-                throw new NotSupportedException("LinqToExcel does not provide support for the First() method");
+                throw new NotSupportedException("LinqToGmail does not provide support for the First() method");
             else if (resultOperator is MaxResultOperator)
-                throw new NotSupportedException("LinqToExcel does not provide support for the Max() method");
+                throw new NotSupportedException("LinqToGmail does not provide support for the Max() method");
             else if (resultOperator is MinResultOperator)
-                throw new NotSupportedException("LinqToExcel does not provide support for the Min() method");
+                throw new NotSupportedException("LinqToGmail does not provide support for the Min() method");
             else if (resultOperator is SumResultOperator)
-                throw new NotSupportedException("LinqToExcel does not provide support for the Sum() method");
+                throw new NotSupportedException("LinqToGmail does not provide support for the Sum() method");
             else if (resultOperator is ContainsResultOperator)
-                throw new NotSupportedException("LinqToExcel does not provide support for the Contains() method");
+                throw new NotSupportedException("LinqToGmail does not provide support for the Contains() method");
             else if (resultOperator is DefaultIfEmptyResultOperator)
-                throw new NotSupportedException("LinqToExcel does not provide support for the DefaultIfEmpty() method");
+                throw new NotSupportedException("LinqToGmail does not provide support for the DefaultIfEmpty() method");
             else if (resultOperator is DistinctResultOperator)
-                throw new NotSupportedException("LinqToExcel does not provide support for the Distinct() method");
+                throw new NotSupportedException("LinqToGmail does not provide support for the Distinct() method");
             else if (resultOperator is ExceptResultOperator)
-                throw new NotSupportedException("LinqToExcel does not provide support for the Except() method");
+                throw new NotSupportedException("LinqToGmail does not provide support for the Except() method");
             else if (resultOperator is GroupResultOperator)
-                throw new NotSupportedException("LinqToExcel does not provide support for the Group() method");
+                throw new NotSupportedException("LinqToGmail does not provide support for the Group() method");
             else if (resultOperator is IntersectResultOperator)
-                throw new NotSupportedException("LinqToExcel does not provide support for the Intersect() method");
+                throw new NotSupportedException("LinqToGmail does not provide support for the Intersect() method");
             else if (resultOperator is OfTypeResultOperator)
-                throw new NotSupportedException("LinqToExcel does not provide support for the OfType() method");
+                throw new NotSupportedException("LinqToGmail does not provide support for the OfType() method");
             else if (resultOperator is SingleResultOperator)
-                throw new NotSupportedException("LinqToExcel does not provide support for the Single() method. Use the First() method instead");
+                throw new NotSupportedException("LinqToGmail does not provide support for the Single() method. Use the First() method instead");
             else if (resultOperator is UnionResultOperator)
-                throw new NotSupportedException("LinqToExcel does not provide support for the Union() method");
+                throw new NotSupportedException("LinqToGmail does not provide support for the Union() method");
 
             base.VisitResultOperator(resultOperator, queryModel, index);
         }
